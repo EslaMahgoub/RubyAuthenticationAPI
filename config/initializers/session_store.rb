3c1c0ded1363e02b345bf0eb21c7e0cb493a6d00
exit <exit>
2021-09-18 18:50:11 +0200

    Add registeration
    Add login and logout functionalities

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,1 @@
-Rails.application.config.session_store :cookie_store, key: "_authentican_app", domain: "http://localhost:3000/"
+Rails.application.config.session_store :cookie_store, key: "_authentican_app"
